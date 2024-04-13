0b8c21978f0ceef95ee6401c3479018cc0e506f7
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-04-13 20:03:44 -0300

    feat: add component badge

---
+++ dashboard/src/components/FeedbackCard/Badge.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  type: String
+})
+
+const typeNormalizedToLowerCase = computed(() => props.type.toLocaleLowerCase())
+
+const LABELS = {
+  issue: 'Problema',
+  idea: 'Ideia',
+  other: 'Outro'
+}
+
+const COLORS = {
+  issue: { bg: 'bg-brand-danger' },
+  idea: { bg: 'bg-brand-warning' },
+  other: { bg: 'bg-brand-graydark' }
+}
+
+const badgeLabel = computed(() => LABELS[typeNormalizedToLowerCase.value])
+const badgeClass = computed(() => COLORS[typeNormalizedToLowerCase.value].bg)
+
+</script>
+
+<template>
+  <span class="px-2 py-1 font-black rounded-full text-xs text-white uppercase w-20 text-center" :class="badgeClass">
+    {{ badgeLabel }}
+  </span>
+</template>
--- dashboard/src/views/Feedbacks/FeedbacksList.vue
+++ dashboard/src/views/Feedbacks/FeedbacksList.vue
@@ -1,68 +1,39 @@
 <script setup>
 import { ptBR } from 'date-fns/locale/pt-BR'
 import { formatDistanceToNow } from 'date-fns'
+import Badge from '../../components/FeedbackCard/Badge.vue';
 
 defineProps({
   feedbacks: Array,
 })
 
-function getFilterTypeColorAndText(type) {
-  switch (type) {
-    case "ISSUE":
-      return {
-        color: "bg-red-500",
-        text: "Problemas"
-      }
-    case "IDEA":
-      return {
-        color: "bg-green-500",
-        text: "Ideias"
-      }
-    case "OTHER":
-      return {
-        color: "bg-blue-500",
-        text: "Outros"
-      }
-    default:
-      return {
-        color: "bg-gray-500",
-        text: "Todos"
-      }
-  }
-}
-
 </script>
 <template>
-<div 
-    v-for="feedback in feedbacks" :key="feedback.id"
-    class="py-4 px-6 space-y-4 bg-gray-50 rounded"
-  >
-  <header class="flex items-center justify-between">
-    <span class="px-2 py-1 font-black rounded-full text-xs text-white uppercase"
-        :class="getFilterTypeColorAndText(feedback.type).color">
-        {{ feedback.type }}
-    </span>
-    <time class="text-sm text-gray-500 font-regular" :datetime="new Date(feedback.created_at).toISOString()">
+  <!-- SPLIT THIS COMPONENT FeedbackCard in a separe component -->
+  <div v-for="feedback in feedbacks" :key="feedback.id" class="py-4 px-6 space-y-4 bg-gray-50 rounded">
+    <header class="flex items-center justify-between">
+      <badge :type="feedback.type" />
+      <time class="text-sm text-gray-500 font-regular" :datetime="new Date(feedback.created_at).toISOString()">
         {{ formatDistanceToNow(new Date(feedback.created_at), { locale: ptBR, addSuffix: true }) }}
-    </time>
-  </header>
+      </time>
+    </header>
 
-  <strong class="block font-medium text-lg">
-    {{ feedback.text }}
-  </strong>
-  <div class="grid grid-cols-2">
-    <div class="flex flex-col">
+    <strong class="block font-medium text-lg">
+      {{ feedback.text }}
+    </strong>
+    <div class="grid grid-cols-2">
+      <div class="flex flex-col">
         <p class="uppercase text-xs font-bold text-gray-500">página</p>
-        <a class="font-medium text-sm">{{ feedback.type }}</a>
-    </div>
-    <div class="flex flex-col">
+        <a class="font-medium text-sm">{{ feedback.page }}</a>
+      </div>
+      <div class="flex flex-col">
         <p class="uppercase text-xs font-bold text-gray-500">Usuário</p>
         <p class="font-medium text-sm truncate max-w-xs">{{ feedback.fingersprint }}</p>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <p class="uppercase text-xs font-bold text-gray-500">dispositivo</p>
+      <p class="font-medium text-sm">{{ feedback.device }}</p>
     </div>
   </div>
-  <div class="flex flex-col">
-    <p class="uppercase text-xs font-bold text-gray-500">dispositivo</p>
-    <p class="font-medium text-sm">{{ feedback.device }}</p>
-  </div>
-</div>
 </template>
